b75df80e281659ce4a65435b24ee51c7d0d68bd8
Blister315 <blisterr315@gmail.com>
2025-11-21 18:53:29 +0530

    Initial commit

---
+++ AI-yoga-coach-main/frontend/src/pages/About/About.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import './About.css'
+
+export default function About() {
+    return (
+        <div className="about-container">
+            <h1 className="about-heading">About</h1>
+            <div className="about-main">
+                <p className="about-content">
+                Yoga poses training involves practicing and learning various yoga postures or asanas in order to
+                 improve strength, flexibility, balance, and overall physical and mental well-being. 
+                 There are many different styles of yoga, and each has its own set of asanas.
+                 </p>
+
+                 <p className="about-content">
+                 Discover a smarter way to practice yoga with our AI-powered Yoga Pose Detection App. 
+                 Designed for beginners and experienced yogis alike, the app uses advanced pose-recognition technology to identify your posture in real time and instantly display the name of the yoga pose you’re performing.
+                 Whether you're refining alignment, learning new poses, or tracking your progress, the app offers a simple and intuitive way to enhance your practice.
+                 Stay mindful, improve accuracy, and elevate your yoga journey—with guidance that adapts to you.
+                </p>
+                <div className="developer-info">
+                    <h4>Team members</h4>
+                    <p className="about-content">
+                        
+                            <ol>Harshit Gaur</ol>
+                            <ol>Rajat Sahay</ol>
+                            <ol>Lovenish Kharb</ol>
+                    </p>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
